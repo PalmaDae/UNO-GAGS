8ca8b0156f31f6e4e4e46aa7db87f1b0bc56a688
PalmaDae <arturd277@mail.ru>
2025-12-05 21:41:33 +0300

    from tornado to javafx

--- src/main/java/view/CreateView.kt
+++ src/main/java/view/CreateView.kt
@@ -1,4 +1,12 @@
 package view
 
-class CreateView {
+import controller.CreateController
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+class CreateView(private val stage: Stage) {
+    private val controller = CreateController()
+
+
+
 }
