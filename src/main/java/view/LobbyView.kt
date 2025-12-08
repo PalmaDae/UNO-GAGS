bb0a321e25eeaca7ccdce0bcd48bbafe4e082619
PalmaDae <arturd277@mail.ru>
2025-12-08 17:02:17 +0300

    minimal base

--- src/main/java/view/LobbyView.kt
+++ src/main/java/view/LobbyView.kt
@@ -11,5 +11,13 @@ import jdk.internal.vm.ThreadContainers.root
 class LobbyView(private val stage: Stage, private val rules: List<Boolean>) {
     lateinit var scene: Scene;
 
+    init {
+        val root = VBox(16.0).apply {
+            alignment = Pos.CENTER
+        }
 
+        root.children.add(Label("Dae, OWNER"))
+
+        scene = Scene(root, 400.0, 600.0)
+    }
 }
