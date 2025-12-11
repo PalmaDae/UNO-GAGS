62cdc75b88ede5579cac160e19cfd67684ad0a93
PalmaDae <arturd277@mail.ru>
2025-12-11 17:18:25 +0300

    cards and pass

--- src/main/java/controller/JoinController.kt
+++ src/main/java/controller/JoinController.kt
@@ -1,5 +1,17 @@
 package controller
 
-class JoinController {
+import javafx.scene.Scene
+import javafx.stage.Stage
 
+class JoinController(private val stage: Stage) {
+    private val controller = JoinController(stage)
+    lateinit var scene: Scene
+
+    fun joinGame() {
+
+    }
+
+    fun backTo() {
+
+    }
 }
--- src/main/java/view/LobbyView.kt
+++ src/main/java/view/LobbyView.kt
@@ -2,27 +2,47 @@ package view
 
 import config.StageConfig
 import controller.LobbyController
-import enity.Player
 import javafx.geometry.Pos
 import javafx.scene.Scene
 import javafx.scene.control.Button
 import javafx.scene.control.Label
+import javafx.scene.control.TextField
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
-import jdk.internal.vm.ThreadContainers.root
 
 class LobbyView(private val stage: Stage, private val rules: List<Boolean>) {
-    lateinit var scene: Scene;
-    private val controller = LobbyController(stage);
+    lateinit var scene: Scene
+    private val controller = LobbyController(stage)
 
     init {
-        val startButton = Button("Start game").apply { setOnAction { controller.startGame() } }
+        val password = controller.generatePassword()
 
-        val root = VBox(16.0, startButton).apply {
+        val passwordField = TextField(password).apply {
+            isEditable = false
             alignment = Pos.CENTER
+            prefWidth = 160.0
+            maxWidth = 160.0
+            minWidth = 160.0
+            styleClass.add("uno-input")
         }
 
-        root.children.add(Label("Dae, OWNER"))
+        val copyButton = Button("Copy").apply {
+            setOnAction { controller.copyPassword(password) }
+        }
+
+        val startButton = Button("Start game").apply {
+            setOnAction { controller.startGame() }
+        }
+
+        val root = VBox(16.0).apply {
+            alignment = Pos.CENTER
+            children.addAll(
+                Label("Lobby Owner"),
+                passwordField,
+                copyButton,
+                startButton
+            )
+        }
 
         scene = Scene(root, StageConfig.getWidth(stage), StageConfig.getHeight(stage))
         scene.stylesheets.add(javaClass.getResource("/css/style.css").toExternalForm())
